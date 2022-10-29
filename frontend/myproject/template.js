8097bd830c8266a149136ea3d1a7f844f0085b86
hamitmizrak <hamitmizrak@gmail.com>
2022-10-29 13:16:08 +0300

    validation

--- frontend/myproject/template.js
+++ frontend/myproject/template.js
@@ -11,3 +11,32 @@ let darkLight = () => {
     //alert("dark light")
     document.body.classList.toggle("dark_mode");
 }
+
+
+// register 
+$(function () {
+    $("#btnSumit").click(function () {
+        //variable
+        let register_username, register_email, register_password;
+
+        //trim
+        register_username = jQuery.trim($("#username").val());
+        register_email = jQuery.trim($("#email").val());
+        register_password = jQuery.trim($("#password").val());
+
+        //validation email
+        if (register_username == "") {
+            $("#validation_username").html("Kullanıcı adı boş geçilemez")
+        }
+
+        if (register_email == "") {
+            $("#validation_email").html("Kullanıcı adı boş geçilemez")
+        }
+
+        if (register_password == "") {
+            $("#validation_password").html("Kullanıcı adı boş geçilemez")
+        }
+
+    })
+});
+
