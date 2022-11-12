820bde957290153502c7621753267729c505cf39
hamitmizrak <hamitmizrak@gmail.com>
2022-11-12 09:42:04 +0300

    Exception

---
+++ src/main/java/com/hamitmizrak/javase/HamitMizrakException.java
@@ -0,0 +1,8 @@
+package com.hamitmizrak.javase;
+
+public class HamitMizrakException extends Exception {
+    //Alt+Ins
+    public HamitMizrakException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/hamitmizrak/javase/_21_ExceptionHandling.java
@@ -0,0 +1,26 @@
+package com.hamitmizrak.javase;
+
+
+import java.io.IOException;
+
+public class _21_ExceptionHandling {
+
+    public static void main(String[] args) throws ArithmeticException, IOException {
+
+        int  vocabulary=0;
+        try{
+              vocabulary=44/0;
+        }catch (ArithmeticException arithmeticException){
+            //arithmeticException.printStackTrace();
+            //throw new ArithmeticException("istisna meydana geldi burada dur "+arithmeticException);
+            //throw new HamitMizrakException("benim yazdığım istisna burada dur");
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            System.out.println("database.close()");
+        }
+
+        System.out.println(vocabulary);
+        System.out.println("son satır");
+    }
+}
