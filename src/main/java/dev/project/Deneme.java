05d1a070a4eae161cc6ad586546d9c1f34171033
hamitmizrak <hamitmizrak@gmail.com>
2022-11-19 09:48:09 +0300

    ATM Enum (MoneyCurrency)

---
+++ src/main/java/dev/project/Deneme.java
@@ -0,0 +1,4 @@
+package dev.project;
+
+public class Deneme {
+}
---
+++ src/main/java/dev/project/atm/login/RegisterStaticData.java
@@ -0,0 +1,7 @@
+package dev.project.atm.login;
+
+public class RegisterStaticData {
+    //web sayfası(username,password) ve ATM ekranında(password)
+    public static final String USERNAME="hamit";
+    public static final String PASSWORD="root";
+}
