d28c82eb92fd0efd497e49fc6aaf0f9fd1661315
hamitmizrak <hamitmizrak@gmail.com>
2022-11-19 10:01:41 +0300

    ATM interface

---
+++ src/main/java/dev/project/atm/bank/IAtm.java
@@ -0,0 +1,24 @@
+package dev.project.atm.bank;
+
+// interface [ Access-Modifier(public-friendly) ve static field verebiliriz. )
+// Dummy Code: interface kullanmayacağız başka classlara implement edeceğimiz gövdesiz metotlardır.
+public interface IAtm {
+
+    // para ekle
+    public void addMoney();
+
+    // para görüntüle
+    public void showMoney();
+
+    // para çekmek
+    public void reduceMoney();
+
+    // Havale yapmak
+    public void sendTransactionMoney();
+
+    // EFT yapmak
+    public void sendEFTMoney();
+
+    // Mail Göndermek
+    public void mailSend(String emailAddress);
+}
