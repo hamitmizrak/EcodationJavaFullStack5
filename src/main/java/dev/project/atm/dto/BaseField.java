6ba720098e909fe3a5589e10e0939ca785cbb1bd
hamitmizrak <hamitmizrak@gmail.com>
2022-11-19 10:21:00 +0300

    BaseField revize

--- src/main/java/dev/project/atm/dto/BaseField.java
+++ src/main/java/dev/project/atm/dto/BaseField.java
@@ -2,22 +2,35 @@ package dev.project.atm.dto;
 
 import lombok.Data;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 //lombok
 @Data
 abstract public class BaseField {
     //field
     private Long id;
-    private Date date;
+    private String date;
 
+    // constructorda: overloading yaptık
     //parametresiz constructor
     public BaseField() {
+        date=getNowDate();
     }
 
     //parametreli constructor
-    public BaseField(Long id, Date date) {
+    public BaseField(Long id) {
         this.id = id;
-        this.date = date;
+        date=getNowDate();
+    }
+
+    // now date
+    public String getNowDate(){
+        Locale locale=new Locale("tr","TR");
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("dd-MMMM-yyyy HH:mm:ss",locale);
+        Date date1=new Date();
+        String format=simpleDateFormat.format(date1);
+        return format;
     }
 }
---
+++ src/main/java/dev/project/atm/dto/EftDto.java
@@ -0,0 +1,7 @@
+package dev.project.atm.dto;
+
+//İmportant:
+// Classlarda sadece 1 tane public vardır.
+// Javada classlarda sadece 1 tane extends kullanabilirsiniz.
+public class EftDto extends BaseField{
+}
