048ce4123c81f52cef53f022b79d80dfe36743ce
hamitmizrak <hamitmizrak@gmail.com>
2022-11-19 10:32:15 +0300

    EftDto

--- src/main/java/dev/project/atm/dto/EftDto.java
+++ src/main/java/dev/project/atm/dto/EftDto.java
@@ -3,5 +3,37 @@ package dev.project.atm.dto;
 //İmportant:
 // Classlarda sadece 1 tane public vardır.
 // Javada classlarda sadece 1 tane extends kullanabilirsiniz.
-public class EftDto extends BaseField{
+
+import lombok.Data;
+
+//lombok
+@Data
+public class EftDto extends BaseField {
+
+    //Field
+    private String eftName;
+    private String eftAmount;
+
+    //parametresiz constructor
+    public EftDto() {
+    }
+
+    //parametresiz constructor
+    public EftDto(Long id, String eftName, String eftAmount) {
+        super(id);
+        this.eftName=eftName;
+        this.eftAmount=eftAmount;
+    }
+    //toString
+    @Override
+    public String toString() {
+        return "EftDto{" +
+                "eftName='" + eftName + '\'' +
+                ", eftAmount='" + eftAmount + '\'' +
+                "} " + super.toString();
+    }
+    //my special diffent codes
+    public void specialCode(){
+        System.out.println(new EftDto().hashCode());
+    }
 }
