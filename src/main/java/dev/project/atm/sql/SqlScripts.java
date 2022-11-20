294903107c0c1412075746358437d8adf3462b2f
hamitmizrak <hamitmizrak@gmail.com>
2022-11-20 09:48:57 +0300

    sql scripts

---
+++ src/main/java/dev/project/atm/sql/SqlScripts.java
@@ -0,0 +1,28 @@
+package dev.project.atm.sql;
+
+public class SqlScripts {
+
+    /*
+    https://www.w3schools.com/sql/sql_select.asp
+-- CREATA DATABASE
+    CREATE SCHEMA `atm_database` DEFAULT CHARACTER SET utf8 COLLATE utf8_turkish_ci ;
+    use atm_database;
+
+-- CREATE TABLE
+     CREATE TABLE `atm_database`.`safe` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `money_type` VARCHAR(10) NOT NULL DEFAULT 'madeni',
+    `money_currency` VARCHAR(45) NOT NULL DEFAULT 'tr',
+    `money_amount` VARCHAR(45) NOT NULL DEFAULT '1000',
+    `created_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+     PRIMARY KEY (`id`))
+     ENGINE = InnoDB
+     DEFAULT CHARACTER SET = utf8
+     COLLATE = utf8_turkish_ci;
+
+-- DROP DATABASE
+    DROP DATABASE `atm_database`;
+
+
+     */
+}
