fed0dbc5303a303d41b70c87f3dd23828a1f3176
hamitmizrak <hamitmizrak@gmail.com>
2022-11-26 11:58:31 +0300

    MVC @Controller Object

--- src/main/java/com/hamitmizrak/controller/mvc/ControllerThymeleaf.java
+++ src/main/java/com/hamitmizrak/controller/mvc/ControllerThymeleaf.java
@@ -4,9 +4,11 @@ import com.hamitmizrak.business.dto.RegisterDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/controller")
 public class ControllerThymeleaf {
 
     // http://localhost:8080/thymeleaf1
